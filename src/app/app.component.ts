8f5be8290d615079f1c378647a97a3615a6216d0
Ashwin Sathian <ashwin#developly.com>
2019-03-26 15:54:16 +0530

    First Build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCalc';
+
+  subText = '';
+  mainText = '';
+  operand1: number;
+  operand2: number;
+  operator = '';
+  calculationString = '';
+  answered = false;
+
+  pressKey(key: string) {
+    if (this.answered) {
+      this.allClear();
+      this.answered = false;
+    }
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+      this.operand1 = parseFloat(this.mainText);
+      this.operator = key;
+    }
+    this.mainText += key;
+  }
+
+  allClear() {
+    this.mainText = '';
+    this.subText = '';
+  }
+
+  getAnswer() {
+    this.calculationString = this.mainText;
+    this.operand2 = parseFloat(this.mainText.split(this.operator)[1]);
+    if (this.operator === '/') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 / this.operand2).toString();
+      this.subText = this.calculationString;
+    } else if (this.operator === 'x') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 * this.operand2).toString();
+      this.subText = this.calculationString;
+    } else if (this.operator === '-') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 - this.operand2).toString();
+      this.subText = this.calculationString;
+    } else if (this.operator === '+') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 + this.operand2).toString();
+      this.subText = this.calculationString;
+    } else {
+      this.subText = 'ERROR: Invalid Operation';
+    }
+    this.answered = true;
+  }
 }

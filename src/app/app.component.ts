39841a57a412157b75d1b7d40fb422a38c87f267
Ashwin Sathian <ashwin#developly.com>
2019-03-26 23:31:21 +0530

    Range Improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,19 +14,23 @@ export class AppComponent {
   operator = '';
   calculationString = '';
   answered = false;
+  operatorSet = false;
 
   pressKey(key: string) {
-    if (this.answered) {
-      this.allClear();
-      this.answered = false;
-    }
     if (key === '/' || key === 'x' || key === '-' || key === '+') {
       const lastKey = this.mainText[this.mainText.length - 1];
-      if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+') {
+      if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+')  {
+        this.operatorSet = true;
+      }
+      if ((this.operatorSet) || (this.mainText === '')) {
         return;
       }
       this.operand1 = parseFloat(this.mainText);
       this.operator = key;
+      this.operatorSet = true;
+    }
+    if (this.mainText.length === 10) {
+      return;
     }
     this.mainText += key;
   }
@@ -34,6 +38,7 @@ export class AppComponent {
   allClear() {
     this.mainText = '';
     this.subText = '';
+    this.operatorSet = false;
   }
 
   getAnswer() {
@@ -43,10 +48,17 @@ export class AppComponent {
       this.subText = this.mainText;
       this.mainText = (this.operand1 / this.operand2).toString();
       this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = this.mainText.substr(0, 9);
+      }
     } else if (this.operator === 'x') {
       this.subText = this.mainText;
       this.mainText = (this.operand1 * this.operand2).toString();
       this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
     } else if (this.operator === '-') {
       this.subText = this.mainText;
       this.mainText = (this.operand1 - this.operand2).toString();
@@ -55,6 +67,10 @@ export class AppComponent {
       this.subText = this.mainText;
       this.mainText = (this.operand1 + this.operand2).toString();
       this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
     } else {
       this.subText = 'ERROR: Invalid Operation';
     }
